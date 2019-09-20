d1392953476dc6f4bd1d37d8651261d88f49911c
FawGamer <raphael.maria@outlook.com>
2019-09-20 16:08:32 -0300

    Update Setup_Server.sh

--- Linux/Setup_Server.sh
+++ Linux/Setup_Server.sh
@@ -139,8 +139,8 @@ ipa-client-install --mkhomedir --force --fixed-primary
 
 ### Configuracao de rede do CentOS / Fedora
 nmcli con show
-nmcli connection modify $INTERFACE ipv4.method manual ipv4.addresses $IPADDRESS/16 ipv4.gateway 192.168.8.1 ipv4.dns 192.168.8.100,192.168.8.110 ipv4.dns-search o2pos.com.br
-nmcli connection up $INTERFACE
+nmcli connection modify "System eth0" ipv4.method manual ipv4.addresses 192.168.8.121/16 ipv4.gateway 192.168.8.1 ipv4.dns 192.168.8.100,192.168.8.110 ipv4.dns-search o2pos.com.br
+nmcli connection up "System eth0"
 
 sed -i 's/^IPADDR=x.x.x.x/IPADDR=$ipaddress' /etc/sysconfig/network-scripts/$interface
 sed -i 's/^Domain = localdomain/Domain = o2pos.com/' /etc/idmapd.conf
