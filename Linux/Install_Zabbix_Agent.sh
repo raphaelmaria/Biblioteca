ec69a59a9a06d96d8cbdbd10fc4b3ea0f0873a0f
FawGamer <raphael.maria@outlook.com>
2019-11-26 18:11:34 -0300

    update script zabbix

--- Linux/Install_Zabbix_Agent.sh
+++ Linux/Install_Zabbix_Agent.sh
@@ -13,7 +13,7 @@ rpm -ivh http://repo.zabbix.com/zabbix/2.4/rhel/6/x86_64/zabbix-release-2.4-1.el
 yum -y install zabbix-agent.x86_64
 service zabbix-agent stop
 sed -i 's/^Server=127.0.0.1/Server=192.168.8.4/' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/^Hostname=Zabbix server/Hostname=SRVO2OVT02/' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/^Hostname=Athena/Hostname=SRVO2OVT01/' /etc/zabbix/zabbix_agentd.conf
 chkconfig zabbix on
 chkconfig zabbix-agent on
 mkdir /var/run/zabbix
