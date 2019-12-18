5215ecb0777646f6f21b60e5807c6396f1a3e896
FawGamer <raphael.maria@outlook.com>
2019-12-18 10:27:19 -0300

    update script

--- Linux/Setup_Server.sh
+++ Linux/Setup_Server.sh
@@ -136,8 +136,8 @@ ipa-client-install --mkhomedir --force --fixed-primary
 
 ### Configuracao de rede do CentOS / Fedora
 nmcli con show
-nmcli connection modify ens1f1 ipv4.method manual ipv4.addresses 192.168.8.59/16 ipv4.gateway 192.168.8.1 ipv4.dns 192.168.8.100,192.168.8.110 ipv4.dns-search o2pos.com.br
-nmcli connection up ens1f1
+nmcli connection modify eth0 ipv4.method manual ipv4.addresses 192.168.8.63/16 ipv4.gateway 192.168.8.1 ipv4.dns 192.168.8.100,192.168.8.110 ipv4.dns-search o2pos.com.br
+nmcli connection up eth0
 
 sed -i 's/^IPADDR=x.x.x.x/IPADDR=$ipaddress' /etc/sysconfig/network-scripts/$interface
 sed -i 's/^Domain = localdomain/Domain = o2pos.com/' /etc/idmapd.conf
--- Linux/install_licsrv_houdini.sh
+++ Linux/install_licsrv_houdini.sh
@@ -7,7 +7,7 @@ cd /root/Downloads
 tar -xvf houdini-17.5.173-linux_x86_64_gcc6.3.tar.gz
 bash /houdini-17.5.173-linux_x86_64_gcc6.3/houdini.install -y
 /etc/init.d/sesinetd stop
-scp o2@192.168.8.84:/Volumes/3TB/Downloads/Linux/H17/H17_sesinet/Linux/sesinetd /root
+scp raphaelmaria@192.168.8.84:/Volumes/3TB/Downloads/Linux/H17/H17_sesinet/Linux/sesinetd /root
 rm -rf /usr/lib/sesi/sesinetd
 mv /root/sesinetd /usr/lib/sesi
 chmod -R 755 /usr/lib/sesi/sesinetd
