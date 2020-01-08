0a2d1168d4a03d4ae79f622903db8667ce37929a
FawGamer <raphael.maria@outlook.com>
2020-01-08 11:21:33 -0300

    update data script boris

--- Linux/Install_Boris_FX.sh
+++ Linux/Install_Boris_FX.sh
@@ -9,6 +9,8 @@
 cd
 echo " Efetuando Download dos instaladores"
 yum -y install wget
+yum -y install compat-db.x86_64
+
 wget http://192.168.8.7/app/plugins/Boris/Mocha2020/MochaPro2020-7.0.2-69.ga4c281e9b481.x86_64.rpm
 
 wget http://192.168.8.7/app/plugins/Boris/Sapphire2020/SapphireOFX-2020.000-1.0.x86_64.rpm
@@ -23,6 +25,11 @@ rm -rf /etc/opt/bis
 
 ln -s /etc/opt/isl /etc/opt/bis
 ln -s /etc/opt/isl/licenses /etc/opt/isl/licences
+
+export genarts_LICENSE=/etc/opt/isl/licenses
+
+echo "HOST ATLAS any 5053" >> /usr/genarts/rlm/mocha2020.lic
+echo "HOST Atlas 0019d13d785f 5053" >> /usr/genarts/rlm/sapphire.lic
 
 
 
