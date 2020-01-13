cb420d6c0fe361ec9bc638de4306426b68b3deef
FawGamer <raphael.maria@outlook.com>
2020-01-13 10:29:59 -0300

    add functions

--- Linux/Install_Grafana_CentOS.sh
+++ Linux/Install_Grafana_CentOS.sh
@@ -4,14 +4,37 @@
 # CRIADO POR RAPHAEL MARIA
 # 13 DE JANEIRO DE 2020
 
-
+# INSTALANDO O GRAFANA
 yum update -y
 yum install https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-5.1.0-1.x86_64.rpm
 yum install wget -y
 yum -y install initscripts fontconfig
 rpm -Uvh grafana-5.1.0-1.x86_64.rpm
-yum install -y fontconfig
-yum install -y freetype*
-yum install -y urw-fonts
+yum -y install fontconfig.*
+yum -y install freetype.*
+yum -y install -y urw-fonts
 sudo /sbin/chkconfig --add grafana-server
 sudo service grafana-server start
+
+# ALTERANDO PORTA DO GRAFANA PARA 3000
+systemctl start firewalld
+firewall-cmd --get-default-zone
+firewall-cmd --set-default-zone=public
+firewall-cmd --permanent --add-port=3000/tcp
+firewall-cmd --permanent --add-port=80/tcp
+firewall-cmd --reload
+firewall-cmd --list-all
+
+sudo setcap 'cap_net_bind_service=+ep' /usr/sbin/grafana-server 
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
+
+
+
+
+
+# INSTALANDO PLUGINS UTEIS
+grafana-cli plugins install raintank-worldping-app
+grafana-cli plugins install alexanderzobnin-zabbix-app
+grafana-cli plugins install grafana-clock-panel
+grafana-cli plugins install ntop-ntopng-datasource
+
