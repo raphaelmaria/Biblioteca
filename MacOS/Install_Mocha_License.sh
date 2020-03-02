d80c1696ef5e098a42db9aafca318db86d907b99
FawGamer <raphael.maria@outlook.com>
2020-03-02 15:01:36 -0300

    Upload Version 1

---
+++ MacOS/Install_Mocha_License.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+##########
+#
+# SCRIPT DE INSTALACAO DE LICENCA DO MOCHA PRO FOR ADOBE PLUGIN
+# 
+# CRIADO POR RAPHAEL MARIA
+# EM 02 DE MARCO DE 2020
+# VERSAO 1.0
+
+sudo mkdir -p /Library/Application Support/GenArts/rlm/ << EOF
+O22013M@c
+EOF
+
+cd /Library/Application Support/GenArts/rlm/
+wget http://192.168.8.7/app/plugins/Boris/Mocha2020/Client_Float/floating-client.lic
