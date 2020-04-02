ede552cfb64200ca42d1c900cd143cdad5abb7ac
FawGamer <raphael.maria@outlook.com>
2020-04-02 18:12:06 -0300

    New Version

--- Linux/Install_MySQL_CentOS.sh
+++ Linux/Install_MySQL_CentOS.sh
@@ -6,4 +6,11 @@ yum auto-cleaner
 
 
 
-vbackup
+
+
+
+mysql -uroot -p "create database zabbix character set utf8 collate utf8_bin;"
+mysql -uroot -p "grant all privileges on zabbix.* to zabbix@localhost identified by 'password';"
+mysql -uroot -p "
+
+ALTER TABLE hostmacro CONVERT TO CHARACTER SET utf8 COLLATE utf8_bin;
