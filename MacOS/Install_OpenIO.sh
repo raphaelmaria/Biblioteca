3ff538588320cad05f13818a43cf5ef734907826
FawGamer <raphael.maria@outlook.com>
2020-04-09 13:27:17 -0300

    New Version OpenIO Color

---
+++ MacOS/Install_OpenIO.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew install wget
+brew install opencolorio
+brew install opencolorio --with-python
