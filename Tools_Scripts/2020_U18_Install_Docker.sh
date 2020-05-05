e6c0fea2b3d166dfffac84933884df940c0d4f10
FawGamer <raphael.maria@outlook.com>
2020-05-05 16:11:10 -0300

    update commit Docker install

--- Tools_Scripts/2020_U18_Install_Docker.sh
+++ Tools_Scripts/2020_U18_Install_Docker.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get remove docker docker-engine docker.io
-sudo apt install docker.io
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+
+sudo apt install docker-ce
+sudo systemctl status docker
 sudo systemctl start docker
 sudo systemctl enable docker
-docker --version
+
+# sudo usermod -aG docker ${USER}
