53c09d4f6b0cc4a3e31de55c92a71c279211c38c
raphaelmaria <raphael.maria@outlook.com>
2020-05-26 11:15:39 -0300

    Deploy New Version

--- Linux/install_jitsi_ubuntu18lts.sh
+++ Linux/install_jitsi_ubuntu18lts.sh
@@ -7,16 +7,31 @@
 # AUTHOR: Raphael Maria <http://raphaelmaria.com.br>
 # VERSAO: 0.0.1
 # LICENCA: LICENSE GPL <http://gnu.org/licenses/gpl.html>
+#################################################################
+# Variaveis
+DOMAIN=o2pos.com.br
+HOSTNAME=meet
+VARIP=192.168.8.68
+GATEWAY=192.168.8.1
+DNS1=192.168.8.100
+DNS2=192.168.8.110
+#################################################################
+sudo hostnamectl set-hostname $HOSTNAME.$DOMAIN
+sudo echo "127.0.0.1 localhost $HOSTNAME.$DOMAIN" >> /etc/hosts
 
+nmcli con show
+echo "Qual a placa de rede?"
+read varinterface
 
-sudo hostnamectl set-hostname meet.o2pos.com.br
-sudo echo "127.0.0.1 localhost meet.o2pos.com.br" >> /etc/hosts
-
+nmcli con modify $varinterface ipv4.method manual ipv4.addresses $VARIP/16 ipv4.gateway $GATEWAY ipv4.dns $DNS1,$DNS2 ipv4.dns-search $DOMAIN
+nmcli con up $varinterface
 # Required packages and repository updates
 echo "Required packages and repository updates"
 sudo apt update
 sudo apt -y install apt-transport-https nginx
 sudo apt-add-repository universe
+sudo apt list --upgradable
+sudo apt upgrade -y
 
 curl https://download.jitsi.org/jitsi-key.gpg.key | sudo sh -c 'gpg --dearmor > /usr/share/keyrings/jitsi-keyring.gpg'
 echo 'deb [signed-by=/usr/share/keyrings/jitsi-keyring.gpg] https://download.jitsi.org stable/' | sudo tee /etc/apt/sources.list.d/jitsi-stable.list > /dev/null
@@ -41,7 +56,7 @@ echo "Basic Jitsi Meet install"
 #sudo apt-get -y install jitsi-videobridge
 #sudo apt-get -y install jicofo
 #sudo apt-get -y install jigasi
-sudo apt install jitsi-meet
+sudo apt -y install jitsi-meet
 sudo /usr/share/jitsi-meet/scripts/install-letsencrypt-cert.sh
 sudo apt install jigasi
 
