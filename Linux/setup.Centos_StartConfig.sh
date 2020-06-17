952995928cfd4725b4971480a787ff383ed7c22c
raphaelmaria <raphael.maria@outlook.com>
2020-06-17 19:37:53 -0300

    Update Line

--- Linux/setup.Centos_StartConfig.sh
+++ Linux/setup.Centos_StartConfig.sh
@@ -8,6 +8,9 @@
 # VERSAO: 0.0.1
 # LICENCA: LICENSE GPL <http://gnu.org/licenses/gpl.html>
 
+# Instalação de softwares básicos.
+yum -y install dialog wget tar unzip vim make gcc dnf autoconf automake epel-release 
+
 ##### VARIAVEIS
 VARHOSTNAME=$(dialog --stdout --inputbox 'Insira o nome  do hostname desta maquina: ' 0 0)
 VARIPADDRESS=$(dialog --stdout --inputbox 'Insira o IP ADDRESS do hostname desta maquina: ' 0 0)
@@ -18,8 +21,7 @@ VARINTERFACE=$(nmcli con show | tail -1 | awk '{print $1}')
 nmcli con modify $VARINTERFACE ipv4.method manual ipv4.addresses $VARIPADDRESS/24
 nmcli con up $VARINTERFACE
 
-# Instalação de softwares básicos.
-yum -y install wget tar unzip vim make gcc dnf autoconf automake epel-release 
+# Fazendo update de todo os sistema operacional
 yum -y upgrade
 yum -y update
 
@@ -28,3 +30,5 @@ yum -y install cockpit
 systemctl enable --now cockpit.socket
 firewall-cmd --permanent --zone=public --add-service=cockpit
 firewall-cmd --reload
+
+dialog --msgbox "Acesse este dispositivo através do endereço \n https://$VARIPADDRESS:9090  \n usando o usuário de login desta sessão do shell" 5 10
