ff04ed003757777bc4c672a4ad1117d991e0ce5f
raphaelmaria <raphael.maria@outlook.com>
2020-06-17 22:04:19 -0300

    Update - Remove SUDO in command

--- Linux/setup.Debian_StartConfig.sh
+++ Linux/setup.Debian_StartConfig.sh
@@ -9,7 +9,7 @@
 # LICENCA: LICENSE GPL <http://gnu.org/licenses/gpl.html>
 
 # Instalação de softwares básicos.
-sudo apt -y install dialog wget tar unzip vim make gcc dnf autoconf automake
+apt -y install dialog wget tar unzip vim make gcc dnf autoconf automake
 
 ##### VARIAVEIS
 VARHOSTNAME=$(dialog --stdout --inputbox 'Insira o nome  do hostname desta maquina: ' 0 0)
@@ -25,13 +25,14 @@ nmcli con up $VARINTERFACE
 # Instala o Dashboard WEB Red Hat Cockpit
 echo 'deb http://deb.debian.org/debian stretch-backports main' > \
  /etc/apt/sources.list.d/backports.list
+
 apt-get update -y
-sudo apt-get install cockpit
+apt-get install cockpit
 systemctl enable --now cockpit.socket
 
 # Fazendo update de todo os sistema operacional
-sudo apt -y upgrade
-sudo apt -y update
+apt -y upgrade
+apt -y update
 
 
 dialog --msgbox "Sua configuração iniciar foi concluida \nAcesse este dispositivo através do endereço \nhttps://$VARIPADDRESS:9090  \nusando o usuário de logado desta sessão do shell" 0 0
