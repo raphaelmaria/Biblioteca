a81c4e93998c4ccbbb85b11c18241ed34072c473
Raphael Maria <raphael.maria@outlook.com>
2020-07-28 14:14:52 -0300

    update

--- Linux_em_testes/deploy.Centos_InstallMySql.sh
+++ Linux_em_testes/deploy.Centos_InstallMySql.sh
@@ -2,8 +2,8 @@
 timedatectl set-timezone America/Sao_Paulo
 
 # Instalação de softwares básicos.
-yum -y install dialog wget tar unzip vim gcc dnf epel-release ipa-client
-yum -y install net-tools tcpdump nano curl 
+yum -y install dialog wget tar unzip vim gcc dnf epel-release ipa-client &
+yum -y install net-tools tcpdump nano curl &
 
 ##### VARIAVEIS
 VARHOSTNAME=$(dialog --stdout --inputbox 'Insira o nome  do hostname desta maquina: ' 0 0)
@@ -17,17 +17,17 @@ nmcli con modify $VARINTERFACE ipv4.method manual ipv4.addresses $VARIPADDRESS/1
 nmcli con up $VARINTERFACE
 
 # Instalação do Ansible
-yum -y install ansible  epel-release
-yum provides pip
-yum install python2-pip -y
-pip install pip --upgrade
-pip install ansible
-pip install ansible --upgrade
+yum -y install ansible  epel-release &
+yum provides pip &
+yum install python2-pip -y &
+pip install pip --upgrade &
+pip install ansible &
+pip install ansible --upgrade &
 
 # Setup SELINUX
-setenforce 0
-getenforce
-sestatus
+setenforce 0 &
+getenforce &
+sestatus &
 
 # Setup MariaDB
 dnf -y install mysql-server
@@ -51,11 +51,11 @@ mysql -u root -p << EOF
 8!H58HefmPGU
 create database zabbix character set utf8 collate utf8_bin;
 create user 'zabbix'@'localhost' identified by 'Z@bbix1989';
-grant all privileges on zabbix.* to 'zabbix'@'localhost';
+grant all privileges on zabbix to 'zabbix'@'localhost';
 create user 'zabbix'@'192.168.8.4' identified with mysql_native_password by 'Z@bbix1989';
 grant all privileges on zabbix.* to 'zabbix'@'192.168.8.4';
-UPDATE mysql.user SET Super_Priv='Y' WHERE user='zabbix' AND host='[IP]';
-flush privileges
+UPDATE mysql.user SET Super_Priv='Y' WHERE user='zabbix' AND host='192.168.8.4';
+flush privileges;
 '''
 # Setup Firewalld
 firewall-cmd --permanent --add-port=3306/tcp
