a746a284e7d929d5e8b46552709ef8167348272e
raphaelmaria <raphael.maria@outlook.com>
2020-10-04 14:03:20 -0300

    Update Line Ansible Automation

--- ansible/Setup_Ansible.sh
+++ ansible/Setup_Ansible.sh
@@ -10,6 +10,10 @@
 # FONTE:
 # 
 #####################################################################
+#VARIAVEIS
+GitRepo="https://github.com/raphaelmaria/deploy.git"
+AnsibleDIR= "/etc/ansible"
+#####################################################################
 clear
 PS3="> Selecione o gerenciador de pacote do seu OS:"
 select OS APT YUM; do
@@ -19,18 +23,41 @@ case $OS in
     APT)
     sudo apt upgrade -y
     sudo apt update -y
-    sudo apt -y install wget vim ansible git htop dstat unzip tar gcc
+    sudo apt -y install dialog wget vim ansible git htop dstat unzip tar gcc python
     cd ~
-    git clone 
+    git clone $GitRepo
+    sudo mv $AnsibleDIR/ansible.conf $AnsibleDIR/ansible.conf.bak
+    sudo mv $AnsibleDIR/hosts $AnsibleDIR/hosts.bak
+    sudo mv deploy/ansible/ansible.conf $AnsibleDIR
+    sudo apt-get install python-pip -y
+    sudo pip2 install pip --upgrade
+    sudo pip2 install ansible
+    sudo pip2 install ansible --upgrade
+    sudo touch $AnsibleDIR/hosts
+
     break ;;
 
     # Instalacao em Sistema Operacionais RedHat/Centos/yum
     YUM)
     sudo yum update -y
     sudo yum upgrade -y
-    sudo yum -y install wget vim ansible git htop dstat unzip tar gcc epel-release
+    sudo yum -y install dialog wget vim ansible git htop dstat unzip tar gcc epel-release python
+    cd ~
+    git clone $GitRepo
+    sudo mv $AnsibleDIR/ansible.conf $AnsibleDIR/ansible.conf.bak
+    sudo mv $AnsibleDIR/hosts $AnsibleDIR/hosts.bak
+    sudo mv deploy/ansible/ansible.conf $AnsibleDIR
+    sudo yum provides pip
+    sudo yum install python2-pip -y
+    sudo pip2 install pip --upgrade
+    sudo pip2 install ansible
+    sudo pip2 install ansible --upgrade
+    sudo touch $AnsibleDIR/hosts
+
     
     break ;;
+dialog --tittle "Sucesso" --infobox "Operação Concluida com Sucesso" 5 35; sleep 4;
+clear
 done
 
 
