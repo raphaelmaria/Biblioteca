be7053e80bb53f9177a346c0991bb918fd01cad0
raphaelmaria <raphael.maria@outlook.com>
2020-11-18 15:28:17 -0300

    Update

---
+++ Linux_Homologados/deploy.Ubuntu_NvidiaDrivers.sh
@@ -0,0 +1,5 @@
+#!/bin/bash/
+sudo apt install build-essential libglvnd-dev pkg-config -y
+sudo apt install dialog wget tar unzip vim make gcc dnf autoconf automake ansible -y
+sudo apt install python3
+sudo pip install ansible -y
