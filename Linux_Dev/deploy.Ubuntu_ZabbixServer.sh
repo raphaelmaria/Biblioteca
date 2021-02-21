c572c70ffb52a6ca4984e805eda91a85b37ce0eb
raphaelmaria <raphael.maria@outlook.com>
2021-02-21 16:10:01 -0300

    Add Zabbix Deploy Raspberry

---
+++ Linux_Dev/deploy.Ubuntu_ZabbixServer.sh
@@ -0,0 +1,5 @@
+Ubuntu 20.04
+
+wget https://repo.zabbix.com/zabbix/5.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.2-1+ubuntu20.04_all.deb
+dpkg -i zabbix-release_5.2-1+ubuntu20.04_all.deb
+apt update
