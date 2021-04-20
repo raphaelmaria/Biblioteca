62ecbabaf6001c6d5a0937c91d5ac8da68b58ee9
FawGamer <raphael.maria@outlook.com>
2021-04-20 12:43:45 -0300

    Update Fix PostScript

--- loft/Start_PostInstall.bat
+++ loft/Start_PostInstall.bat
@@ -1,5 +1,5 @@
-powershell.exe "C:\Suporte\Install_JumpCloud.ps1"
-powershell.exe "C:\Suporte\crowdstrike-facon-ps.ps1"
+Start-Process -wait powershell -verb runas -ArgumentList "-file C:\Suporte\Install_JumpCloud.ps1"
+Start-Process -wait powershell -verb runas -ArgumentList "-file C:\Suporte\crowdstrike-facon-ps.ps1"
 msg %username% Instalacao de CrowdStrike e JumpCloud efetuadas com sucesso!
 
 del "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\Start_PostInstall.bat"
