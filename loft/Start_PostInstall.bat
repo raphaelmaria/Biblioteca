33ca2d97df9cc5a0aca6d53a884e28a42c2d2672
FawGamer <raphael.maria@outlook.com>
2021-04-26 15:16:37 -0300

    Fix RunAs Powershell

--- loft/Start_PostInstall.bat
+++ loft/Start_PostInstall.bat
@@ -1,5 +1,7 @@
-Start-Process -wait powershell -verb runas -ArgumentList "-file C:\Suporte\Install_JumpCloud.ps1"
-msg %username% Instalacao de CrowdStrike e JumpCloud efetuadas com sucesso!
+Start-Process -wait -FilePath powershell.exe -verb RunAs -ArgumentList "-file C:\Suporte\Install_JumpCloud.ps1"
+Start-Process -wait -FilePath powershell.exe -verb RunAs -ArgumentList "-file C:\Suporte\crowdstrike-facon-ps.ps1"
+
+msg %username% "Instalacao de CrowdStrike e JumpCloud efetuadas com sucesso!"
 
 del "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\Start_PostInstall.bat"
 REM del "C:\Suporte\Install_JumpCloud.ps1"
