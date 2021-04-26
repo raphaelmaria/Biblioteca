ed4b54980964b1a8d073dcbd47abc054573b6494
FawGamer <raphael.maria@outlook.com>
2021-04-26 18:09:05 -0300

    Update

--- loft/Start_PostInstall.bat
+++ loft/Start_PostInstall.bat
@@ -1,7 +1,7 @@
-Start-Process -wait -FilePath powershell.exe -verb RunAs -ArgumentList "-file C:\Suporte\Install_JumpCloud.ps1"
-Start-Process -wait -FilePath powershell.exe -verb RunAs -ArgumentList "-file C:\Suporte\crowdstrike-facon-ps.ps1"
+REM Powershell.exe -Command "& {Start-Process Powershell.exe -Verb RunAs}"
+REM powershell -Command "Start-Process PowerShell -Verb RunAs -f C:\Suporte\Install_JumpCloud.ps1"
+Start-Process powershell -verb runas -ArgumentList "-file C:\Suporte\Install_JumpCloud.ps1"
 
-msg %username% "Instalacao de CrowdStrike e JumpCloud efetuadas com sucesso!"
 
 del "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\Start_PostInstall.bat"
 REM del "C:\Suporte\Install_JumpCloud.ps1"
