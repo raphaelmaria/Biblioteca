7c66554f79d5205e98a33105b3d8c6e7109641e3
FawGamer <raphael.maria@outlook.com>
2021-05-03 18:05:33 -0300

    Update

--- loft/Install_Partial_v2.1.bat
+++ loft/Install_Partial_v2.1.bat
@@ -34,11 +34,16 @@ echo "Removendo Pontos de Restauracao"
 vssadmin delete shadows /All
 @echo =====================================================
 
-@echo "SET WALLPAPER DEFAULT"
+@echo ">_ SET WALLPAPER DEFAULT"
 reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f 
 reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /d C:\Suporte\2021_04_Wallpaper_Project.jpg /f
 reg add "HKEY_CURRENT_USER\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters 
+
+@echo ">_ Registrando Team Viewer Padroes Loft"
+reg import C:\Suporte\TeamViewer_Settings.reg
+reg import C:\Suporte\TeamViewer_Settings.reg /reg:32
+reg import C:\Suporte\TeamViewer_Settings.reg /reg:64
 exit
 
 
