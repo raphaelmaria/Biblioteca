d7b3f0766fd1803306d790e589c3d98c1cae463c
FawGamer <raphael.maria@outlook.com>
2021-05-12 09:08:31 -0300

    Update

--- loft/Install_JumpCloud.ps1
+++ loft/Install_JumpCloud.ps1
@@ -1,7 +1,9 @@
 #if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit}
 #Set-ExecutionPolicy RemoteSigned
-
+Write-Host "###############################################"
 Write-Host "INSTALANDO COMPONENTES .NET FRAMEWORK ESSENCIAIS"
+Write-Host "###############################################"
+
 choco install vcredist2005 -dvfy
 choco install vcredist2008 -dvfy
 choco install vcredist2012 -dvfy
@@ -14,18 +16,26 @@ choco install vcredist140 -dvfy
 $LocalUser = Get-LocalUser -Name "Loft User"
 $Password = (Get-Content C:\Suporte\note.txt) | ConvertTo-SecureString -key (Get-Content C:\Suporte\key.txt)
 $LocalUser | Set-LocalUser -Password $Password
-Write-Host ">_ EFETUADO A TROCA DA SENHA ADMIN COM SUCESSO!..."
-Write-Host " "
-Write-Host " "
+Write-Host "###############################################"
+Write-Host ">_ EFETUADO A TROCA DA SENHA COM SUCESSO!..."
+Write-Host "###############################################"
 
+Write-Host "###############################################"
 Write-Host ">_ INSTALANDO JUMP CLOUD AGENT"
+Write-Host "###############################################"
+
 cd $env:temp | Invoke-Expression; Invoke-RestMethod -Method Get -URI https://raw.githubusercontent.com/TheJumpCloud/support/master/scripts/windows/InstallWindowsAgent.ps1 -OutFile InstallWindowsAgent.ps1 | Invoke-Expression; ./InstallWindowsAgent.ps1 -JumpCloudConnectKey "ef10c8ee36a34a414100d8c1eb2d93f26464acc5"
 Start-Sleep 30   
 
-#           [ELE AGUARDA 15 SEGUNDOS, PORQUE AINDA EM SEGUNDO PLANO, A INSTALACAO ESTA EM ANDAMENTO]              
-Write-Host ">_ Iniciando Servicos do JumpCloud"
+# [INSTALACAO DO ANTIVIRUS CROWNSTRIKE]
+Start-Process -wait powershell -verb runas -ArgumentList "-file C:\Suporte\crowdstrike-facon-ps.ps1"
+Write-Host "###############################################"
+Write-Host ">_ Crowd Strike Falcon INSTALADO COM SUCESSO"
+Write-Host "###############################################"
 
-#           [BUSCA O SERVICO E VERIFICA O STATUS]
+#   [ELE AGUARDA 15 SEGUNDOS, PORQUE AINDA EM SEGUNDO PLANO, A INSTALACAO ESTA EM ANDAMENTO]              
+Write-Host ">_ Iniciando Servicos do JumpCloud"
+#   [BUSCA O SERVICO E VERIFICA O STATUS DO JUMP CLOUD AGENT]
 $JCService = Get-Service -Name "jumpcloud-agent"
 if ($JCService.Status -eq "Running"){                
     Write-Host "###############################################"
@@ -33,14 +43,7 @@ if ($JCService.Status -eq "Running"){
     Write-Host "###############################################"
     Write-Host ">_ INSTALACAO EFETUADA COM SUCESSO! <<<" 
     Write-Host "###############################################"
-    Write-Host " "
-    Write-Host " "
-    start-sleep 15
-    
-    # [INSTALACAO DO ANTIVIRUS CROWNSTRIKE]
-    Start-Process -wait powershell -verb runas -ArgumentList "-file C:\Suporte\crowdstrike-facon-ps.ps1"
-    Write-Host ">_ Crowd Strike Falcon INSTALADO COM SUCESSO"
-    Start-Sleep 5
+    start-sleep 20
     pause
 }else{
     Write-Host "Servico nao iniciou, Reiniciando a maquina"
