cf9172eadde526ca7ebe251f22a6b9e888002fce
fawgamer <raphael.maria@outlook.com>
2021-10-30 17:31:42 -0300

    updat

--- Linux_Homologados/deploy.Debian_CouchPotato.sh
+++ Linux_Homologados/deploy.Debian_CouchPotato.sh
@@ -1,128 +1,252 @@
 #!/bin/sh
 '''
 Links usados para a pesquisa:
-Install Docker: https://www.smarthomebeginner.com/install-docker-on-ubuntu-1604/
-Install Sick Rage: https://www.smarthomebeginner.com/install-couchpotato-using-docker/
-Install Couch Potato: https://couchpota.to/#
-Who is Install: https://www.how2shout.com/linux/how-to-install-couchpotato-on-ubuntu-20-04-lts-linux/
+Install Sickrage: https://www.smarthomebeginner.com/install-sickrage-on-ubuntu/
+Install CoachPotato: https://www.how2shout.com/linux/how-to-install-couchpotato-on-ubuntu-20-04-lts-linux/
+Install Sonarr: https://varhowto.com/install-sonarr-ubuntu-20-04/
+Install Jackett: https://varhowto.com/install-jackett-ubuntu-20-04/
+Install Radarr: https://varhowto.com/install-radarr-ubuntu-20-04/
 '''
-# REMOVE PYTHON VERSION
-$pythonversion = python3 --version
-IF($pythonversion -eq "Python 3.4.10")
-sudo apt purge python3.* 
+# Variaveis necessarias para instalacao
+$userLocal = ${whoime}
 
-# INSTALL PYTHON
-sudo apt autoremove
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install gcc libffi-dev libssl-dev python3-dev
+# Alterando algumas configuracoes
+# Mudando o nome da maquina:
+sudo hostnamectl set-hostname pyserver
+# Mudanro o Time Zone para o horario ficar certo:
+sudo timedatectl set-timezone America/Sao_Paulo
 
-sudo apt-get install build-essential checkinstall -y
-sudo apt-get install software-properties-common -y
-sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev -y
-sudo apt-get install python3-pip -y
-sudo apt-get install build-essential python-dev git scons swig -y
-sudo apt-get install python3-pip python3-setuptools python3-psutil python3-bottle python3-requests -y
-'''
-cd /usr/src
-sudo wget https://www.python.org/ftp/python/3.4.4/Python-3.4.4.tgz
-sudo tar xzf Python-3.4.4.tgz
-cd Python-3.4.4
-sudo ./configure
-sudo make altinstall
-sudo python3.4 -V
-'''
-sudo apt-get install python3-pip -y
-pip install --no-deps -U news-please
+# Atualizando o Debian / Ubuntu
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt --fix-broken -y
+sudo apt autoremove -y
+sudo apt-get upgrade -y
+sudo apt-get install update-manager-core
+do-release-upgrade
 
-# LXML (link: https://lxml.de/installation.html)
-sudo apt autoremove
-sudo apt-get install python3-lxml -y
-sudo apt-get install libxml2-dev libxslt-dev python-dev -y
-sudo apt-get build-dep python3-lxml -y
-sudo pip install lxml
-pip install lxml==3.4.2
+# Instalando app que sao requisitos
+sudo apt -y gcc ansible wget vim git-core
 
-menu()
-{
-clear
-echo "------------------------------------------"
-echo "Criado por Raphael Maria"
-echo "1. Se quer fazer a instalaçao via Copilacacao"
-echo " "
-echo "2. Se quer fazer a instalacao via contanier (docker)"
-echo " "
-echo "3. Para SAIR"
-echo -n "Escolha uma opção: "
-read OPTION
-echo
-case "$OPTION" in
-    1)
-    sudo apt-get autoremove
-    cd ~
-    git https://git.sickrage.ca/SiCKRAGE/sickrage.git
-    cd sickrage
-    python3 -m pip install -r requirements.txt
-    python3 -m pip install -r requirements-dev.txt
+# Instalando o gerenciador via Web Browser
+sudo apt install cockpit -y
+##
+## AGORA SO ACESSAR O NAVEGADOR NO http://localhost:9090/
+##
+# Criando diretorios Necessarios.
+sudo mkdir -p /home/$userLocal/multimidia/{Series,Movies,Animes}
+sudo chmod -R 777 /home/$userLocal/multimidia/*
 
-    #Instalacao Couch Potato
-    apt-get install git-core
-    git clone https://github.com/CouchPotato/CouchPotatoServer.git
-    python CouchPotatoServer/CouchPotato.py
-    sudo cp CouchPotatoServer/init/ubuntu /etc/init.d/couchpotato
-    sudo nano /etc/init.d/couchpotato
-    sudo chmod +x /etc/init.d/couchpotato
-    sudo update-rc.d couchpotato defaults
+# Instalando e configurando o Samba para acesso pela REDE
+sudo apt install samba samba-client
+sudo mv /etc/samba/smb.conf /etc/samba/smb.conf.original
+sudo touch /etc/samba/smb.conf
 
-;;
-    2)
-    # Install DOCKER in Ubuntu
-    sudo apt-get update
-    sudo apt-get install apt-transport-https ca-certificates
-    sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
-    sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-    echo deb https://apt.dockerproject.org/repo ubuntu-xenial main | sudo tee /etc/apt/sources.list.d/docker.list
-    sudo apt-get update
-    apt-cache policy docker-engine
-    sudo apt-get install docker-engine
-    
-    # sudo service docker start ---> Linha do site
-     #Linhas adicionada por Raphael Maria
-    
-    # DEPLOY SICKRAGE
-    sudo mkdir -p /mnt/dados/multimidia/{cache,movies,series,tv,anime}
-    sudo chmod -R 777 /mnt/dados/multimidia
-    docker pull sickrage/sickrage
-    docker run -d --name="sickrage" -v /path/to/sickrage/data:/config -v /path/to/downloads:/home/raphaelmaria/downloads -v /path/to/tv:/multimidia/series -v /path/to/anime:/multimidia/anime -e TZ=America/Sao_Paulo -p 8081:8081 sickrage/sickrage:latest
+sudo cat <<EOF > /etc/samba/smb.conf
+#CRIADO POR RAPHAEL MARIA
 
-    sudo docker start sickrage
-    echo "Basta abri o navegador com o endereço IP desta maquina seguido da porta 8081."
-    echo "IPADDRESS:8091"
-    pause
-    echo "Pressione qualquer tecla para sair!"
-    exit;
-;;
-    3)
-    echo "SAINDO... Até Logo!"
-    sleep 5
-    clear;
-    exit;
-;;
-*)
-    echo "A opção selecionada não existe!"
-    echo "Tente novamente selecione uma opção do menu."
-esac
-done
+[global]
+   workgroup = WORKGROUP
+	server string = %h server (Samba, Ubuntu)
+   dns proxy = no
+   netbios name = "$hostname"
+   
+# Configuracao de Checagem de Ativo na Rede
+	keepalive = 30
 
-}
-menu
+##################################
+#####     LOGS DO SAMBA    #######
+##################################
+    log file = /var/log/samba/%m.log
+    # Tamanho de Log e igual a 200Mb
+    max log size = 200000
+    # Nivel de Coleta de Log
+    log level = 1 auth:5
 
-'''
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-'''
+# Configuracao do Servico de Autenticacao Local
+    server role = standalone server
+    passdb backend = tdbsam
+    obey pam restrictions = yes
+    unix password sync = yes
+    passwd program = /usr/bin/passwd %u
+    passwd chat = *Enter\snew\s*\spassword:* %n\n *Retype\snew\s*\spassword:* %n\n *password\supdated\ssuccessfully* .
+    pam password change = yes
+    map to guest = bad user
+    usershare allow guests = yes
+
+###########################
+# TUNNING SAMBA
+###########################
+    strict locking = no
+    read raw = yes
+    write raw = yes
+    oplocks = yes
+    max xmit = 65535
+    deadtime = 15
+    getwd cache = yes
+    socket options = TCP_NODELAY
+    create mask = 0777
+    directory mask = 0777
+    strict allocate = yes
+    min receivefile size = 16384
+    use sendfile = true
+    aio write size = 16384
+    inherit owner = yes
+    inherit group = yes
+
+###################################   
+# CONFIGURACAO DA LIXEIRA
+###################################
+    vfs objects = full_audit,recycle
+    recycle:version = yes
+    recycle:repository = /var/samba/trash/%U
+    recycle:keeptree = yes
+    recycle:exclude = *.mp3, *.zip, *.rar
+    recycle:exluce_dir = tmp,cache
+
+###################################
+# CONFIGURACAO DE Auditoria (DESATIVADA, para ativar remova os '#" das linhas abaixo)
+###################################
+    #full_audit:success = write, unlink, rename, rmdir, chmod, chown
+    #full_audit:prefix = %u|%S
+    #full_audit:failure = none
+    #full_audit:facility = local5
+    #full_audit:priority = notice
+
+###################################
+# Sessao que apresenta a Definicao a serem usadas no compartilhamento por pasta
+###################################
+
+[printers]
+   comment = All Printers
+   browseable = no
+   path = /var/spool/samba
+   printable = yes
+   guest ok = no
+   read only = yes
+   create mask = 0700
+
+[print$]
+   comment = Printer Drivers
+   path = /var/lib/samba/printers
+   browseable = no
+   read only = yes
+   guest ok = no
+
+###################################
+# CONFIGURACAO DE COMPARTILHAMENTO DA LIXEIRA.
+###################################
+
+[Lixeira]
+	path = /var/samba/trash/%U
+	writable = yes
+	create mask = 0700
+	directory mask = 0700
+	browseable = yes
+
+###################################
+# Pasta que vai aparecer na REDE.
+###################################
+[multimidia]  
+	path = /home/${whoime}/multimidia
+	browseable = yes
+	writable = yes
+	guest ok = yes
+	
+EOF
+
+# Instalando o Sonarr
+# Requisitos
+sudo apt install mono-devel -y
+sudo apt install gnupg ca-certificates -y
+# Instalando o repositorio official.
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493
+echo "deb http://apt.sonarr.tv/ master main" | sudo tee /etc/apt/sources.list.d/sonarr.list
+sudo apt update
+sudo apt install nzbdrone -y
+sudo cat <<EOF > /etc/systemd/system/sonarr.service
+[Unit]
+Description=Sonarr Daemon
+After=network.target
+
+[Service]
+# Change and/or create the required user and group.
+User=$userLocal
+Group=$userLocal
+
+# The UMask parameter controls the permissions of folders and files created.
+#UMask=002
+
+# The -data=/path argument can be used to force the config/db folder
+ExecStart=/usr/bin/mono --debug /opt/NzbDrone/NzbDrone.exe -nobrowser
+
+Type=simple
+TimeoutStopSec=20
+KillMode=process
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl enable --now sonarr.service
+##
+## AGORA SO ACESSAR O NAVEGADOR NO http://localhost:8989/
+##
+
+# Install Jacket
+curl -L -O $( curl -s https://api.github.com/repos/Jackett/Jackett/releases | grep Jackett.Binaries.LinuxAMDx64.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 )
+tar -xvzf Jackett.Binaries.LinuxAMDx64.tar.gz
+sudo mv Jackett /opt
+cd /opt/Jackett
+sudo ./install_service_systemd.sh
+##
+## AGORA SO ACESSAR O NAVEGADOR NO http://localhost:9117/
+##
+
+# Instalando qBittorrent
+sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
+sudo apt-get update && sudo apt-get install qbittorrent
+
+# Instalando o Radarr
+sudo apt install curl mediainfo -y
+curl -L -O $( curl -s https://api.github.com/repos/Radarr/Radarr/releases | grep linux.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 )
+ls Radarr*
+tar -xvzf Radarr.*.linux.tar.gz
+sudo mv Radarr /opt
+sudo mono /opt/Radarr/Radarr.exe
+sudo cat <<EOF > /etc/systemd/system/radarr.service
+[Unit]
+Description=Radarr Daemon
+After=syslog.target network.target
+
+[Service]
+# Change and/or create the required user and group.
+User=raphaelmaria
+Group=raphaelmaria
+
+Type=simple
+
+# Change the path to Radarr or mono here if it is in a different location for you.
+ExecStart=/usr/bin/mono --debug /home/vh/Radarr/Radarr.exe -nobrowser
+TimeoutStopSec=20
+KillMode=process
+Restart=on-failure
+
+# These lines optionally isolate (sandbox) Radarr from the rest of the system.
+# Make sure to add any paths it might use to the list below (space-separated).
+#ReadWritePaths=/opt/Radarr /path/to/movies/folder
+#ProtectSystem=strict
+#PrivateDevices=true
+#ProtectHome=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl enable --now radarr.service
+
+##
+## AGORA SO ACESSAR O NAVEGADOR NO http://localhost:7878/
+##
