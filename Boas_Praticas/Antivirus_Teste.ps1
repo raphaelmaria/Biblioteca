f48ca835e16f86c49983f378d7fc0d36c773cbc7
fawgamer <raphael.maria@outlook.com>
2022-05-12 18:07:00 -0300

    12/05/22 - Commit Kaira III - Teste de Antivirus

---
+++ Boas_Praticas/Antivirus_Teste.ps1
@@ -0,0 +1,5 @@
+Start-Process microsoftedge.exe http://www.eicar.org/download/eicar.com
+Start-Process microsoftedge.exe http://www.eicar.org/download/eicar.com.txt
+Start-Process microsoftedge.exe http://www.eicar.org/download/eicar_com.zip
+Start-Process microsoftedge.exe http://www.eicar.org/download/eicarcom2.zip
+Start-Process microsoftedge.exe http://www.spycar.org/
