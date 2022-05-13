4fc2fc082e8cc22da3f6e1ebbad463c3802b57e4
fawgamer <raphael.maria@outlook.com>
2022-05-12 21:35:27 -0300

    12/05/22 - Commit Kaira III - CrediHome

--- Boas_Praticas/Antivirus_Teste.ps1
+++ Boas_Praticas/Antivirus_Teste.ps1
@@ -3,3 +3,7 @@ Start-Process microsoftedge.exe http://www.eicar.org/download/eicar.com.txt
 Start-Process microsoftedge.exe http://www.eicar.org/download/eicar_com.zip
 Start-Process microsoftedge.exe http://www.eicar.org/download/eicarcom2.zip
 Start-Process microsoftedge.exe http://www.spycar.org/
+
+
+Start-Process microsoftedge.exe https://www.spyshelter.com/security-test-tool/
+Start-Process microsoftedge.exe https://www.wicar.org/test-malware.html (Bloqueado por ambos) 
