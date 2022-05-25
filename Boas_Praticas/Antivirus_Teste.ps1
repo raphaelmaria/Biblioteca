b806c96eeb8f995cba06a1be95dbdfa98517fd21
fawgamer <raphael.maria@outlook.com>
2022-05-25 00:59:12 -0300

    Autocommit

--- Boas_Praticas/Antivirus_Teste.ps1
+++ Boas_Praticas/Antivirus_Teste.ps1
@@ -7,3 +7,7 @@ Start-Process microsoftedge.exe http://www.spycar.org/
 
 Start-Process microsoftedge.exe https://www.spyshelter.com/security-test-tool/
 Start-Process microsoftedge.exe https://www.wicar.org/test-malware.html (Bloqueado por ambos) 
+
+bazaar.abuse.ch/
+virus total.com
+VMFile
