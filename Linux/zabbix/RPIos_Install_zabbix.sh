7f2395094d7d6aeaca940f7c0107397b2c43ddf3
fawgamer <raphael.maria@outlook.com>
2022-05-26 23:21:50 -0300

    Instalacao zabbix em Raspberry

--- Linux/zabbix/RPIos_Install_zabbix.sh
+++ Linux/zabbix/RPIos_Install_zabbix.sh
@@ -30,17 +30,20 @@ y
 y
 EOF
 
-mysql -uroot -p'rootDBpass' -e "create database zabbix character set utf8mb4 collate utf8mb4_bin;"
-mysql -uroot -p'rootDBpass' -e "grant all privileges on zabbix.* to zabbix@localhost identified by 'zabbixDBpass';"
+mysql -uroot -p'password' -e "create database zabbix character set utf8mb4 collate utf8mb4_bin;"
+mysql -uroot -p'password' -e "grant all privileges on zabbix.* to zabbix@localhost identified by 'zabbixDBpass';"
 
 sudo zcat /usr/share/doc/zabbix-sql-scripts/mysql/server.sql.gz | mysql -uzabbix -p'zabbixDBpass' zabbix
-sudo see 's/DBPassword=\DBPassword=zabbixDBpass\' /etc/zabbix/zabbix_server.conf
+sudo sed -i 's/^DBPassword=/DBPassword=zabbixDBpass/' /etc/zabbix/zabbix_server.conf
 sudo systemctl restart zabbix-server zabbix-agent
 sudo systemctl enable zabbix-server zabbix-agent
 
-sudo sed 's/php_value date.timezone Europe/Amsterdam\php_value date.timezone America/Sao_Paulo\' /etc/zabbix/apache.conf 
+sudo sed -i '20c\php_value date.timezone America/Sao_Paulo/' /etc/zabbix/apache.conf 
 sudo systemctl restart apache2
 sudo systemctl enable apache2
+
+# https://ora-600.com/2022/04/zabbix-locale-for-language-en_us-is-not-found-on-the-web-server-tried-to-set-en_us-en_us-utf8/
+
 
 echo "Acesso IP/zabbix"
 echo "User: Admin"
