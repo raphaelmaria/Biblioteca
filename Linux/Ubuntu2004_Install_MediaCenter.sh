ead7edf64f35badf52f1d7b622e5055168280034
raphaelmaria <raphael.maria@outlook.com>
2022-05-29 01:23:13 -0300

    auto

--- Linux/Ubuntu2004_Install_MediaCenter.sh
+++ Linux/Ubuntu2004_Install_MediaCenter.sh
@@ -7,7 +7,7 @@ sudo apt install dialog tree wget tar unzip vim make gcc autoconf automake git -
 sudo apt autoremove
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install gcc libffi-dev libssl-dev python3-dev -y
+
 sudo apt-get install build-essential checkinstall -y
 sudo apt-get install software-properties-common -y
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev -y
