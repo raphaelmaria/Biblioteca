07fc65ed40fe13e98c76efae26d886040e14f8f1
raphaelmaria <raphael.maria@outlook.com>
2023-03-02 17:44:21 -0300

    Add

--- Linux/ubuntu_install_SambaFileSystem.sh
+++ Linux/ubuntu_install_SambaFileSystem.sh
@@ -1,13 +1,13 @@
-    #!/usr/bin/env bash
+#!/usr/bin/env bash
+sudo apt -y install dialog
+sudo apt -y install vlc-plugin-samba vlc smbclient samba-vfs-modules samba samba-dev samba-dsdb-modules samba-libs
+sudo mv /etc/samba/smb.conf /etc/samba/smb.conf.bkp
+
 VARSHARE=$(dialog --stdout --inputbox 'Insira o nome  do compartilhamento: ' 0 0)
 VARPATH=$(dialog --stdout --inputbox 'Insira o diretorio: ' 0 0)
 
 sudo mkdir $VARPATH/$VARSHARE
 sudo chmod -R 775 $VARPATH/$VARSHARE
-
-sudo apt -y install vlc-plugin-samba vlc smbclient samba-vfs-modules samba samba-dev samba-dsdb-modules samba-libs
-sudo mv /etc/samba/smb.conf /etc/samba/smb.conf.bkp
-
 
 cat <<EOF >
  # See smb.conf.example for a more detailed config file or
@@ -22,18 +22,14 @@ cat <<EOF >
 	map to guest = Bad User
 	guest ok = yes
 
-##################################
-#####     LOGS DO SAMBA    #######
-##################################
+#>_ Logs
 log file = /var/log/samba/%m.log
 # Tamanho de Log e igual a 200Mb
 max log size = 200000
 # Nivel de Coleta de Log
 log level = 1 auth:5
 
-###################################
-####       TUNNING SAMBA     ######
-###################################
+#>_ TUNNING SAMBA
 strict locking = no
 read raw = yes
 write raw = yes
@@ -54,8 +50,8 @@ socket options = TCP_NODELAY
 	recycle:repository = Lixeira
 	recycle:keeptree = yes
 	recycle:versions = yes
-	EOF
+EOF
 
-    sudo testparm
-    sudo systemctl enable --now smbd.service
-    sudo systemctl enable --now nmbd.service
+sudo testparm
+sudo systemctl enable --now smbd.service
+sudo systemctl enable --now nmbd.service
