34428886a0e91685c7d8eb8c40e189bceb3f3601
raphaelmaria <raphael.maria@outlook.com>
2023-04-13 17:01:41 -0300

    add

--- Linux/Ubuntu_Install_Docker.sh
+++ Linux/Ubuntu_Install_Docker.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 #Fonte: https://docs.docker.com/engine/install/ubuntu/
-sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get -y remove docker docker-engine docker.io containerd runc
 sudo apt-get update
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+sudo apt-get -y install ca-certificates curl gnupg vim lsb-release
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
