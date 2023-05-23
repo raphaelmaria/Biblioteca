2a7c184283af50c6d74f4b5bd798eb10bc4e0d1b
raphaelmaria <raphael.maria@outlook.com>
2023-05-23 18:21:52 -0300

    Add Install Suricata

--- Linux/Debian_Install_Suricata.sh
+++ Linux/Debian_Install_Suricata.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
+sudo apt -y install curl vim 
+
+sudo add-apt-repository ppa:oisf/suricata-stable
 echo "deb http://deb.debian.org/debian bullseye-blackports main" > /etc/apt/source.list.d/backports.list
 
 sudo apt update
 
 sudo apt -y install suricata suricata-update -t bullseye-backports
+
+sudo apt -y install network-manager
+INTERFACE=$(nmcli device show | awk '/DEVICE/ {print $2}' | head -n 1)
+echo "Escreva seu escopo de rede local. Ex.: 192.168.x.x/24"
+read RANGEIP
+
+
+sed -i '19 s/^/#/' /etc/default/suricata && sed -i '20 a\IFACE='$INTERFACE'' /etc/default/suricata
+sed -i '18 s/^/#/' /etc/suricata/suricata.yaml && sed -i '19 a\HOME_NET: "['$RANGEIP']"' /etc/suricata/suricata.yaml
+sed -i '24 s/^/#/' /etc/suricata/suricata.yaml && sed -i '25 a\EXTERNAL_NET: "any"' /etc/suricata/suricata.yaml
+
