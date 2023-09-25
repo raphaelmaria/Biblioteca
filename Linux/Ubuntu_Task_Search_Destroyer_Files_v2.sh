b7e68690defa9474c314d382b94abf26c1a5a89a
raphaelmaria <raphael.maria@outlook.com>
2023-09-25 00:25:12 -0300

    Add

---
+++ Linux/Ubuntu_Task_Search_Destroyer_Files_v2.sh
@@ -0,0 +1,7 @@
+# Localizar todos os arquivos .nfo e listar seus caminhos
+find /caminho/para/a/pasta/x -type f -name "*.nfo" | while read -r file; do
+    echo "Caminho do arquivo encontrado: $file"
+done
+
+# Apagar todos os arquivos .nfo
+find /caminho/para/a/pasta/x -type f -name "*.nfo" -exec rm {} \;
