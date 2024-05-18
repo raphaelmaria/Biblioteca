46849b347235f5edda4e78a72d629fa249023336
raphaelmaria <raphael.maria@outlook.com>
2024-05-18 01:44:33 -0300

    V1.2 - Incluir consoles

---
+++ .history/GameStick/Y3Lite/CreatePlaylist_20240518004416.ps1
@@ -0,0 +1,48 @@
+#MUDE A LETRA DA UNIDADE AQUI:
+$
+
+# FAZENDO BACKUP DA PLAYLIST ATUAL
+
+
+# Caminho da pasta contendo os arquivos
+$unidUSB = "F:"
+$console = "ps1"
+
+$caminho = "$unidUSB\roms\$console"
+Write-Host "Rodando em cima do caminho: $caminho"
+
+# Verifica se o caminho da pasta é válido
+if (-not (Test-Path $caminho -PathType Container)) {
+    Write-Host "O caminho da pasta não é válido."
+    exit
+}
+
+# Obtém a lista de arquivos na pasta, excluindo arquivos com a extensão .png
+$arquivos = Get-ChildItem $caminho | Where-Object { $_.Extension -ne ".png" }
+
+# Inicializa uma lista para armazenar os nomes formatados dos arquivos
+$listaArquivos = @()
+
+# Itera sobre cada arquivo na pasta
+foreach ($arquivo in $arquivos) {
+    # Obtém o nome do arquivo (sem extensão)
+    $nome = $arquivo.BaseName
+    
+    # Obtém a extensão do arquivo
+    $extensao = $arquivo.Extension
+    
+    # Formata o nome do arquivo
+    $nomeFormatado = "$nome$extensao <$nome>"
+    
+    # Adiciona o nome formatado à lista
+    $listaArquivos += $nomeFormatado
+}
+
+
+# Caminho do arquivo de saída
+$arquivoSaida = "$unidUSB\playlist\$console.pl"
+
+# Escreve a lista formatada no arquivo de saída
+$listaArquivos | Out-File -FilePath $arquivoSaida -Encoding utf8
+
+Write-Host "Lista de arquivos foi salva em $arquivoSaida."
--- GameStick/Y3Lite/CreatePlaylist.ps1
+++ GameStick/Y3Lite/CreatePlaylist.ps1
@@ -1,9 +1,369 @@
-# Caminho da pasta contendo os arquivos
-$unidUSB = "F:"
-$console = "ps1"
+#MUDE A LETRA DA UNIDADE AQUI:
+#$unidUSB="f:"
+# Função para listar todas as unidades USB
+function Get-USBDrives {
+    Get-PSDrive -PSProvider FileSystem | Where-Object { $_.Root -match '^[A-Z]:\\$' }
+}
 
-$caminho = "$unidUSB\roms\$console"
-Write-Host "Rodando em cima do caminho: $caminho"
+# Listar as unidades USB disponíveis
+$usbDrives = Get-USBDrives
+
+# Mostrar as unidades USB disponíveis ao usuário
+Write-Host "Unidades USB disponíveis:"
+foreach ($drive in $usbDrives) {
+    Write-Host "$($drive.Name): $($drive.Root)"
+}
+
+# Solicitar que o usuário selecione uma unidade
+$unidUSB = Read-Host "Digite a letra da unidade USB com a qual deseja trabalhar (ex: E)"
+
+# Verificar se a unidade selecionada é válida
+if ($usbDrives.Name -contains $unidUSB) {
+    $unidUSB = "${unidUSB}:"
+    Write-Host "Você selecionou a unidade USB: $unidUSB"
+} else {
+    Write-Host "Unidade USB inválida. Por favor, execute o script novamente e selecione uma unidade válida."
+}
+
+# VARIAVEIS IMPORTANTES -- NÃƒO MEXE
+$pathPlaylist="$unidUSB\playlist"
+$pathATARI="$unidUSB\roms\atari"
+$pathCPS="$unidUSB\roms\cps"
+$pathFC="$unidUSB\roms\fc"
+$pathGB="$unidUSB\roms\gb"
+$pathGBC="$unidUSB\roms\gbc"
+$pathGBA="$unidUSB\roms\gba"
+$pathMD="$unidUSB\roms\md"
+$pathPS1="$unidUSB\roms\ps1"
+$pathSFC="$unidUSB\roms\sfc"
+$pathSMS="$unidUSB\roms\sms"
+$pathZ64="$unidUSB\roms\z64"
+
+
+# MENU
+function Show-Menu {
+ param (
+ [string] $Title = "Atualizar Playlist Y3 Lite - v1.0"
+ )
+ write-Host $Title
+ Write-Host "Pressione '1' para atualizar todas"
+ Write-Host "Pressione '2' para atualizar ATARI"
+ Write-Host "Pressione '3' para atualizar ARCADE/MAME"
+ Write-Host "Pressione '4' para atualizar NINTENDINHO"
+ Write-Host "Pressione '5' para atualizar GameBoy"
+ Write-Host "Pressione '6' para atualizar GameBoy Color"
+ Write-Host "Pressione '7' para atualizar GameBoy Advanced"
+ Write-Host "Pressione '8' para atualizar MegaDrive"
+ Write-Host "Pressione '9' para atualizar Playstation One"
+ Write-Host "Pressione '10' para atualizar Super Nintendo"
+ Write-Host "Pressione '11' para atualizar Master System"
+ Write-Host "Pressione '12' para atualizar Nintendo 64"
+ Write-Host "Pressione 'q' para sair"
+
+ }do{
+    Show-Menu
+    $selection = Read-Host "Qual a licença desejada: "
+    switch ($selection)
+    {
+        '1'{
+        Rename-Item $unidUSB\playlist $unidUSB\playlistOLD
+        New-Item -Path $unidUSB\playlist -ItemType Directory
+
+        #----------------------------------
+        } '2' {
+         if (Test-Path -Path $unidUSB\playlist\atariOLD.pl) {
+             Remove-Item -Path $unidUSB\playlist\atariOLD.pl -Force
+            }
+
+            Rename-Item $unidUSB\playlist\atari.pl $unidUSB\playlist\atariOLD.pl
+        
+            if (-not (Test-Path $pathATARI -PathType Container)) {
+                Write-Host "O caminho da pasta não é válido."
+            exit
+            }
+
+            $arquivos = Get-ChildItem $caminho | Where-Object { $_.Extension -ne ".png" }
+            $listaArquivos = @()
+
+            foreach ($arquivo in $arquivos) {
+            $nome = $arquivo.BaseName
+            $extensao = $arquivo.Extension
+            $nomeFormatado = "$nome$extensao <$nome>"
+            $listaArquivos += $nomeFormatado
+            }
+        
+            $arquivoSaida = "$unidUSB\playlist\atari.pl"
+            $listaArquivos | Out-File -FilePath $arquivoSaida -Encoding utf8
+            Write-Host "Lista de arquivos do ATARI foi salva em $arquivoSaida."
+        #----------------------------------
+        } '3' {
+            if (Test-Path -Path $unidUSB\playlist\cpsOLD.pl) {
+                Remove-Item -Path $unidUSB\playlist\cpsOLD.pl -Force
+            }
+
+            Rename-Item $unidUSB\playlist\cps.pl $unidUSB\playlist\cps.pl
+        
+            if (-not (Test-Path $pathCPS -PathType Container)) {
+                Write-Host "O caminho da pasta não é válido."
+            exit
+            }
+
+            $arquivos = Get-ChildItem $caminho | Where-Object { $_.Extension -ne ".png" }
+            $listaArquivos = @()
+
+            foreach ($arquivo in $arquivos) {
+            $nome = $arquivo.BaseName
+            $extensao = $arquivo.Extension
+            $nomeFormatado = "$nome$extensao <$nome>"
+            $listaArquivos += $nomeFormatado
+            }
+        
+            $arquivoSaida = "$unidUSB\playlist\cps.pl"
+            $listaArquivos | Out-File -FilePath $arquivoSaida -Encoding utf8
+            Write-Host "Lista de arquivos do MAME foi salva em $arquivoSaida."
+        #----------------------------------
+        } '4' {
+          if (Test-Path -Path $unidUSB\playlist\fcOLD.pl) {
+        Remove-Item -Path $unidUSB\playlist\fcOLD.pl -Force
+        }
+
+        Rename-Item $unidUSB\playlist\fc.pl $unidUSB\playlist\fcOLD.pl
+        
+        if (-not (Test-Path $pathFC -PathType Container)) {
+            Write-Host "O caminho da pasta não é válido."
+        exit
+        }
+
+        $arquivos = Get-ChildItem $caminho | Where-Object { $_.Extension -ne ".png" }
+        $listaArquivos = @()
+
+        foreach ($arquivo in $arquivos) {
+        $nome = $arquivo.BaseName
+        $extensao = $arquivo.Extension
+        $nomeFormatado = "$nome$extensao <$nome>"
+        $listaArquivos += $nomeFormatado
+        }
+        
+        $arquivoSaida = "$unidUSB\playlist\fc.pl"
+        $listaArquivos | Out-File -FilePath $arquivoSaida -Encoding utf8
+        Write-Host "Lista de arquivos do NINTENDINHO foi salva em $arquivoSaida."
+        #-------------------------------------
+        } '5' {
+           if (Test-Path -Path $unidUSB\playlist\gbOLD.pl) {
+        Remove-Item -Path $unidUSB\playlist\gbOLD.pl -Force
+        }
+
+        Rename-Item $unidUSB\playlist\gb.pl $unidUSB\playlist\gbOLD.pl
+        
+        if (-not (Test-Path $pathGB -PathType Container)) {
+            Write-Host "O caminho da pasta não é válido."
+        exit
+        }
+
+        $arquivos = Get-ChildItem $caminho | Where-Object { $_.Extension -ne ".png" }
+        $listaArquivos = @()
+
+        foreach ($arquivo in $arquivos) {
+        $nome = $arquivo.BaseName
+        $extensao = $arquivo.Extension
+        $nomeFormatado = "$nome$extensao <$nome>"
+        $listaArquivos += $nomeFormatado
+        }
+        
+        $arquivoSaida = "$unidUSB\playlist\gb.pl"
+        $listaArquivos | Out-File -FilePath $arquivoSaida -Encoding utf8
+        Write-Host "Lista de arquivos do GameBoy foi salva em $arquivoSaida."
+        } '6' {
+            if (Test-Path -Path $unidUSB\playlist\gbcOLD.pl) {
+        Remove-Item -Path $unidUSB\playlist\gbcOLD.pl -Force
+        }
+
+        Rename-Item $unidUSB\playlist\gbc.pl $unidUSB\playlist\gbcOLD.pl
+        
+        if (-not (Test-Path $pathGBC -PathType Container)) {
+            Write-Host "O caminho da pasta não é válido."
+        exit
+        }
+
+        $arquivos = Get-ChildItem $caminho | Where-Object { $_.Extension -ne ".png" }
+        $listaArquivos = @()
+
+        foreach ($arquivo in $arquivos) {
+        $nome = $arquivo.BaseName
+        $extensao = $arquivo.Extension
+        $nomeFormatado = "$nome$extensao <$nome>"
+        $listaArquivos += $nomeFormatado
+        }
+        
+        $arquivoSaida = "$unidUSB\playlist\gbc.pl"
+        $listaArquivos | Out-File -FilePath $arquivoSaida -Encoding utf8
+        Write-Host "Lista de arquivos do GameBoy Color foi salva em $arquivoSaida."
+        #---------------------------------
+        } '7' {
+        if (Test-Path -Path $unidUSB\playlist\gbaOLD.pl) {
+        Remove-Item -Path $unidUSB\playlist\gbaOLD.pl -Force
+        }
+
+        Rename-Item $unidUSB\playlist\gba.pl $unidUSB\playlist\gbaOLD.pl
+        
+        if (-not (Test-Path $pathGBA -PathType Container)) {
+            Write-Host "O caminho da pasta não é válido."
+        exit
+        }
+
+        $arquivos = Get-ChildItem $caminho | Where-Object { $_.Extension -ne ".png" }
+        $listaArquivos = @()
+
+        foreach ($arquivo in $arquivos) {
+        $nome = $arquivo.BaseName
+        $extensao = $arquivo.Extension
+        $nomeFormatado = "$nome$extensao <$nome>"
+        $listaArquivos += $nomeFormatado
+        }
+        
+        $arquivoSaida = "$unidUSB\playlist\gba.pl"
+        $listaArquivos | Out-File -FilePath $arquivoSaida -Encoding utf8
+        Write-Host "Lista de arquivos do GameBoy Advenced foi salva em $arquivoSaida."
+        
+        #------------------------
+        } '8' {
+        if (Test-Path -Path $unidUSB\playlist\mdOLD.pl) {
+        Remove-Item -Path $unidUSB\playlist\mdOLD.pl -Force
+        }
+
+        Rename-Item $unidUSB\playlist\md.pl $unidUSB\playlist\mdOLD.pl
+        
+        if (-not (Test-Path $pathMD -PathType Container)) {
+            Write-Host "O caminho da pasta não é válido."
+        exit
+        }
+
+        $arquivos = Get-ChildItem $caminho | Where-Object { $_.Extension -ne ".png" }
+        $listaArquivos = @()
+
+        foreach ($arquivo in $arquivos) {
+        $nome = $arquivo.BaseName
+        $extensao = $arquivo.Extension
+        $nomeFormatado = "$nome$extensao <$nome>"
+        $listaArquivos += $nomeFormatado
+        }
+        
+        $arquivoSaida = "$unidUSB\playlist\md.pl"
+        $listaArquivos | Out-File -FilePath $arquivoSaida -Encoding utf8
+        Write-Host "Lista de arquivos do MegaDrive foi salva em $arquivoSaida."
+        
+        #----------------------------------
+        } '9' {
+        if (Test-Path -Path $unidUSB\playlist\ps1OLD.pl) {
+        Remove-Item -Path $unidUSB\playlist\ps1OLD.pl -Force
+        }
+
+        Rename-Item $unidUSB\playlist\ps1.pl $unidUSB\playlist\ps1OLD.pl
+        
+        if (-not (Test-Path $pathPS1 -PathType Container)) {
+            Write-Host "O caminho da pasta não é válido."
+        exit
+        }
+
+        $arquivos = Get-ChildItem $caminho | Where-Object { $_.Extension -ne ".png" }
+        $listaArquivos = @()
+
+        foreach ($arquivo in $arquivos) {
+        $nome = $arquivo.BaseName
+        $extensao = $arquivo.Extension
+        $nomeFormatado = "$nome$extensao <$nome>"
+        $listaArquivos += $nomeFormatado
+        }
+        
+        $arquivoSaida = "$unidUSB\playlist\ps1.pl"
+        $listaArquivos | Out-File -FilePath $arquivoSaida -Encoding utf8
+        Write-Host "Lista de arquivos do PS1 foi salva em $arquivoSaida."
+        
+        #------------------------
+        } '10' {
+        if (Test-Path -Path $unidUSB\playlist\sfcOLD.pl) {
+        Remove-Item -Path $unidUSB\playlist\sfcOLD.pl -Force
+        }
+
+        Rename-Item $unidUSB\playlist\sfc.pl $unidUSB\playlist\sfcOLD.pl
+        
+        if (-not (Test-Path $pathSFC -PathType Container)) {
+            Write-Host "O caminho da pasta não é válido."
+        exit
+        }
+
+        $arquivos = Get-ChildItem $caminho | Where-Object { $_.Extension -ne ".png" }
+        $listaArquivos = @()
+
+        foreach ($arquivo in $arquivos) {
+        $nome = $arquivo.BaseName
+        $extensao = $arquivo.Extension
+        $nomeFormatado = "$nome$extensao <$nome>"
+        $listaArquivos += $nomeFormatado
+        }
+        
+        $arquivoSaida = "$unidUSB\playlist\sfc.pl"
+        $listaArquivos | Out-File -FilePath $arquivoSaida -Encoding utf8
+        Write-Host "Lista de arquivos do Super Nintendo foi salva em $arquivoSaida."
+        #------------------
+        } '11' {
+        if (Test-Path -Path $unidUSB\playlist\smsOLD.pl) {
+        Remove-Item -Path $unidUSB\playlist\smsOLD.pl -Force
+        }
+
+        Rename-Item $unidUSB\playlist\sms.pl $unidUSB\playlist\smsOLD.pl
+        
+        if (-not (Test-Path $pathSMS -PathType Container)) {
+            Write-Host "O caminho da pasta não é válido."
+        exit
+        }
+
+        $arquivos = Get-ChildItem $caminho | Where-Object { $_.Extension -ne ".png" }
+        $listaArquivos = @()
+
+        foreach ($arquivo in $arquivos) {
+        $nome = $arquivo.BaseName
+        $extensao = $arquivo.Extension
+        $nomeFormatado = "$nome$extensao <$nome>"
+        $listaArquivos += $nomeFormatado
+        }
+        
+        $arquivoSaida = "$unidUSB\playlist\sms.pl"
+        $listaArquivos | Out-File -FilePath $arquivoSaida -Encoding utf8
+        Write-Host "Lista de arquivos do MasterSystem foi salva em $arquivoSaida."
+        #-------------------------------------------
+        } '12' {
+        if (Test-Path -Path $unidUSB\playlist\z64OLD.pl) {
+        Remove-Item -Path $unidUSB\playlist\z64OLD.pl -Force
+        }
+
+        Rename-Item $unidUSB\playlist\z64.pl $unidUSB\playlist\z64OLD.pl
+        
+        if (-not (Test-Path $pathSMS -PathType Container)) {
+            Write-Host "O caminho da pasta não é válido."
+        exit
+        }
+
+        $arquivos = Get-ChildItem $caminho | Where-Object { $_.Extension -ne ".png" }
+        $listaArquivos = @()
+
+        foreach ($arquivo in $arquivos) {
+        $nome = $arquivo.BaseName
+        $extensao = $arquivo.Extension
+        $nomeFormatado = "$nome$extensao <$nome>"
+        $listaArquivos += $nomeFormatado
+        }
+        
+        $arquivoSaida = "$unidUSB\playlist\z64.pl"
+        $listaArquivos | Out-File -FilePath $arquivoSaida -Encoding utf8
+        Write-Host "Lista de arquivos do Nintendo 64 foi salva em $arquivoSaida."
+           
+        } 
+    
+    }
+}
+until ( $selection -eq 'q' )
+
 
 # Verifica se o caminho da pasta Ã© vÃ¡lido
 if (-not (Test-Path $caminho -PathType Container)) {
