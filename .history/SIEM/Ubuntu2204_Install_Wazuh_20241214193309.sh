e5438af865436ffa59b382ab70227d7466af28a7
raphaelmaria <raphael.maria@outlook.com>
2024-12-14 23:01:32 -0300

    Update

---
+++ .history/SIEM/Ubuntu2204_Install_Wazuh_20241214193309.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+# 
+# Fonte: https://medium.com/@akobeajiboluemmanuel/step-by-step-setup-of-wazuh-siem-on-ubuntu-22-04-3-lts-4663104fe69b
+
+
+curl -sO https://packages.wazuh.com/4.7/wazuh-install.sh && sudo bash ./wazuh-install.sh -a
